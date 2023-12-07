9655cb3a92773dcd3b8ed9d483a3de20b41b635b
Amirmohamad salehi <71013189+amir-m-s@users.noreply.github.com>
2023-12-07 19:40:45 +0330

    Update appwriteConfig.js
    Import Databases and create project, database and messages id variables and get them from appwirte console.

--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -1,4 +1,8 @@
-import { Client } from "appwrite";
+import { Client, Databases } from "appwrite";
+
+export const PROJECT_ID = "6570c320d19d4897747c";
+export const DATABASE_ID = "6570c424b14ca6265502";
+export const COLLECTION_ID_MESSAGES = "6570c4328e09191eaf9d";
 
 // Create a Client instance
 const client = new Client();
@@ -6,5 +10,7 @@ const client = new Client();
 client
   .setEndpoint("https://cloud.appwrite.io/v1")
   .setProject("6570c320d19d4897747c");
+
+export const databases = new Databases(client);
 
 export default client;
