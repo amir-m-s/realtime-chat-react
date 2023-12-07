4b13c3897d95d63d9abc929a57e35a63a17e1a0d
Amirmohamad salehi <71013189+amir-m-s@users.noreply.github.com>
2023-12-07 19:51:33 +0330

    feat: Get messages
    Get messages from server using useState and useEffect hooks

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,5 +1,35 @@
+import { useState, useEffect } from "react";
+import {
+  databases,
+  DATABASE_ID,
+  COLLECTION_ID_MESSAGES,
+} from "../appwriteConfig";
+
 const Room = () => {
-  return <div>Room!</div>;
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    getMessages();
+  }, []);
+
+  const getMessages = async () => {
+    const response = await databases.listDocuments(
+      DATABASE_ID,
+      COLLECTION_ID_MESSAGES
+    );
+
+    console.log(response);
+    setMessages(response.documents);
+  };
+  return (
+    <>
+      {messages.map(message => (
+        <div key={message.$id}>
+          <div>{message.$createdAt}</div>
+          <div>{message.body}</div>
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default Room;
