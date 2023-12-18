5bae90bb53009e10e3dafb44141bd89cde256c1e
Amirmohamad salehi <71013189+amir-m-s@users.noreply.github.com>
2023-12-18 21:24:59 +0330

    feat: control dropdown on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import { useState } from "react";
 import { FaUser } from "react-icons/fa";
 
 const Header = () => {
+  const [userDropDown, setUserDropDown] = useState(false);
+
   return (
     <header className="main-header">
       <div className="container">
@@ -11,10 +13,25 @@ const Header = () => {
           </h2>
         </div>
         <div className="user-control">
-          <div className="user-btn">
-            <button className="btn btn-accent btn-rounded">
+          <div className="user-btns">
+            <button
+              className="btn btn-accent btn-rounded"
+              onClick={() => setUserDropDown(prevState => !prevState)}
+            >
               <FaUser />
             </button>
+          </div>
+          <div
+            className={`dropdown btn-rounded ${
+              userDropDown && "dropdown-visible"
+            }`}
+          >
+            <div className="dropdown-container">
+              <div className="user-info">Username</div>
+              <div className="control-btns">
+                <button className="btn btn-rounded btn-primary">Logout</button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
